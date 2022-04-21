666f00b04228868a75e5a14e6f01169283d62bfb
bahanm <bahanm@gmail.com>
2022-04-20 21:03:22 -0700

    trying to troubleshoot deployment admin site not loading.

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,19 +1,19 @@
 from django import forms
 from .models import Profile
 
-class UserForm(UserCreationForm):
-    email = forms.EmailField()
+# class UserForm(UserCreationForm):
+#     email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
 
 
-class ProfileForm(forms.ModelForm):
+# class ProfileForm(forms.ModelForm):
 
-    class Meta:
-        model = Profile
-        fields = ['location', 'avatar']
-        # widgets = {
-        #     'birthdate': DateInput(attrs={'type': 'date'}),
-        # }
+#     class Meta:
+#         model = Profile
+#         fields = ['location', 'avatar']
+#         # widgets = {
+#         #     'birthdate': DateInput(attrs={'type': 'date'}),
+#         # }
